a5ea6903723406a82e7a027ce3e699ea2dc7d493
Kyle Ledbetter <kyle@kyleledbetter.com>
2011-09-23 17:03:22 -0500

    - adding placeholder markup for project detail view
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- dev/4.0.x/component/site/views/project/tmpl/default.php
+++ dev/4.0.x/component/site/views/project/tmpl/default.php
@@ -22,12 +22,83 @@
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
-<div class="item-page project-project" id="projectfork">
-    <h2><?php echo $this->item->title;?></h2>
-    <ul class="actions">
-        <li><a href="index.php">Edit</a></li>    
-        <li><a href="index.php">Print</a></li>    
-        <li><a href="index.php">Archive</a></li>    
-        <li><a href="index.php">Delete</a></li>
-    </ul>
+<div id="projectfork" class="item-page view-project">
+	<h2>Project Name <input type="button" class="button" value="View Dashboard" /></h2>
+	<ul class="actions">
+						<li class="print-icon">
+			<a rel="nofollow" onclick="window.open(this.href,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no'); return false;" title="Print" href="#"><img alt="Print" src="/projectfork_4/media/system/images/printButton.png"></a>			</li>
+		
+					<li class="email-icon">
+			<a onclick="window.open(this.href,'win2','width=400,height=350,menubar=yes,resizable=yes'); return false;" title="Email" href="#"><img alt="Email" src="/projectfork_4/media/system/images/emailButton.png"></a>			</li>
+		
+					<li class="edit-icon">
+			<span title="" class="hasTip"><a href="#"><img alt="Edit" src="/projectfork_4/media/system/images/edit.png"></a></span>			</li>
+		
+	
+	</ul>
+	<dl class="article-info">
+		<dt class="article-info-term">Details</dt>
+		<dd class="category-name">
+			Company: <a href="#">Company Name</a>		
+		</dd>
+		<dd class="start-date">
+			Started on Saturday, 01 January 2011 00:00	
+		</dd>
+		<dd class="due-date">
+			Due by Saturday, 01 January 2011 00:00	
+		</dd>
+		<dd class="createdby">
+			Created by <a href="#">Firstname Lastname</a>		
+		</dd>
+	</dl>
+	<div id="article-index" class="project-stats">
+		<ul>
+			<li class="milestone-stats">
+				<a class="toclink" href="#">4</a> Milestones
+			</li>
+			<li class="task-stats">
+				<a class="toclink" href="#">25</a> Tasks
+			</li>
+			<li class="file-stats">
+				<a class="toclink" href="#">15</a> Files
+			</li>
+			<li class="comment-stats">
+				<a class="toclink" href="#">54</a> Comments
+			</li>
+			<li class="company-stats">
+				<a class="toclink" href="#">2</a> Companies
+			</li>
+			<li class="department-stats">
+				<a class="toclink" href="#">6</a> Departments
+			</li>
+			<li class="user-stats">
+				<a class="toclink" href="#">12</a> Users
+			</li>
+		</ul>
+	</div>
+	<div class="item-description">
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+		Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.
+		</p>
+	</div>
+	<div class="items-more">
+		<h3>Project Milestones</h3>
+		<ol>
+			<li>
+				<a href="#">Milestone 1</a>
+			</li>
+			<li>
+				<a href="#">Milestone 2</a>
+			</li>
+			<li>
+				<a href="#">Milestone 3</a>
+			</li>
+			<li>
+				<a href="#">Milestone 4</a>
+			</li>
+		</ol>
+	</div>
 </div>
