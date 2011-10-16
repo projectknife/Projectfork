b05d7e77b11b66cbb6405d603dcbd9e58a4a4b00
Tobias Kuhn <tobias@projectfork.net>
2011-10-16 18:14:30 +0200

    Updated language files and added some menu items for views

--- dev/4.0.x/component/site/models/project.php
+++ dev/4.0.x/component/site/models/project.php
@@ -39,8 +39,8 @@ class ProjectforkModelProject extends JModelItem
 	 * @var    string
 	 */
 	protected $_context = 'com_projectfork.project';
-    
-    
+
+
     /**
 	 * Method to auto-populate the model state.
 	 * Note. Calling getState in this method will result in recursion.
@@ -53,10 +53,10 @@ class ProjectforkModelProject extends JModelItem
 		// Load state from the request.
 		$pk = JRequest::getInt('id');
 		$this->setState('project.id', $pk);
-        
+
         $state = JRequest::getInt('state');
         $this->setState('filter.state', $state);
-        
+
         $offset = JRequest::getUInt('limitstart');
 		$this->setState('list.offset', $offset);
 
@@ -64,8 +64,8 @@ class ProjectforkModelProject extends JModelItem
 		$params = $app->getParams();
 		$this->setState('params', $params);
 	}
-    
-    
+
+
     /**
 	 * Method to get project data
 	 *
@@ -79,43 +79,43 @@ class ProjectforkModelProject extends JModelItem
 
 		if(is_null($this->_item))    $this->_item = array();
         if(isset($this->_item[$pk])) return $this->_item[$pk];
-        
+
         // Item not in cache - try to fetch it
         try {
             $db = $this->getDbo();
 			$query = $db->getQuery(true);
-            
+
             $query->select(
-                $this->getState('item.select', 
-                    'p.id, p.asset_id, p.title, p.alias, p.description, '
-                    . 'p.created, p.created_by, p.modified, p.modified_by, p.checked_out, '
-                    . 'p.checked_out_time, p.attribs, p.access, p.state, '
+                $this->getState('item.select',
+                    'p.id, p.asset_id, p.title, p.alias, p.description,'
+                    . 'p.created, p.created_by, p.modified, p.modified_by, p.checked_out,'
+                    . 'p.checked_out_time, p.attribs, p.access, p.state,'
                     . 'p.start_date, p.end_date'
                 )
             );
 			$query->from('#__pf_projects AS p');
-            
+
             // Join on user table.
 			$query->select('u.name AS author');
 			$query->join('LEFT', '#__users AS u on u.id = p.created_by');
-            
+
             // Filter by state.
 			$state = $this->getState('filter.state');
-            
+
             $query->where('(p.state = '.intval($state).')');
-            
+
             $db->setQuery($query);
 			$data = $db->loadObject();
 
 			if($error = $db->getErrorMsg()) throw new Exception($error);
 			if(empty($data)) return JError::raiseError(404,JText::_('COM_PROJECTFORK_ERROR_PROJECT_NOT_FOUND'));
-            
+
             // Convert parameter fields to objects.
 			$registry = new JRegistry;
 			$registry->loadString($data->attribs);
 			$data->params = clone $this->getState('params');
 			$data->params->merge($registry);
-            
+
             $this->_item[$pk] = $data;
         }
         catch (JException $e)
@@ -131,6 +131,6 @@ class ProjectforkModelProject extends JModelItem
 		}
 
 	    return $this->_item[$pk];
-    }    
+    }
 }
 ?>
