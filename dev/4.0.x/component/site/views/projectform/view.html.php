8ad0980877d005608ef078abdf2de55b03dd70c7
eaxs <tkuhn@live.com>
2012-03-22 00:06:55 +0100

    Added frontend project form view

---
+++ dev/4.0.x/component/site/views/projectform/view.html.php
@@ -0,0 +1,149 @@
+<?php
+/**
+* @package   Projectfork
+* @copyright Copyright (C) 2006-2012 Tobias Kuhn. All rights reserved.
+* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.php
+*
+* This file is part of Projectfork.
+*
+* Projectfork is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+*
+* Projectfork is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Projectfork. If not, see <http://www.gnu.org/licenses/gpl.html>.
+**/
+
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+
+/**
+ * Project Form View Class for Projectfork component
+ *
+ */
+class ProjectforkViewProjectForm extends JView
+{
+	protected $form;
+	protected $item;
+	protected $return_page;
+	protected $state;
+
+
+	public function display($tpl = null)
+	{
+		// Initialise variables.
+		$app  = JFactory::getApplication();
+		$user = JFactory::getUser();
+
+
+		// Get model data.
+		$this->state	   = $this->get('State');
+		$this->item		   = $this->get('Item');
+		$this->form		   = $this->get('Form');
+		$this->return_page = $this->get('ReturnPage');
+
+
+        // Permission check.
+		if (empty($this->item->id)) {
+			$authorised = ($user->authorise('core.create', 'com_projectfork') || $user->authorise('project.create', 'com_projectfork'));
+		}
+		else {
+			$authorised = $this->item->params->get('access-edit');
+		}
+
+		if ($authorised !== true) {
+			JError::raiseError(403, JText::_('JERROR_ALERTNOAUTHOR'));
+			return false;
+		}
+
+
+        // Bind form data.
+		if (!empty($this->item)) $this->form->bind($this->item);
+
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseWarning(500, implode("\n", $errors));
+			return false;
+		}
+
+		// Create a shortcut to the parameters.
+		$params	= &$this->state->params;
+
+
+		//Escape strings for HTML output
+		$this->pageclass_sfx = htmlspecialchars($params->get('pageclass_sfx'));
+
+
+		$this->params = $params;
+		$this->user	  = $user;
+
+
+        // Prepare the document
+		$this->_prepareDocument();
+
+
+        // Display the view
+		parent::display($tpl);
+	}
+
+
+	/**
+	 * Prepares the document
+     *
+	 */
+	protected function _prepareDocument()
+	{
+		$app		= JFactory::getApplication();
+		$menus		= $app->getMenu();
+		$pathway	= $app->getPathway();
+		$title 		= null;
+
+		// Because the application sets a default page title,
+		// we need to get it from the menu item itself
+		$menu = $menus->getActive();
+		if ($menu)
+		{
+			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
+		} else {
+			$this->params->def('page_heading', JText::_('COM_CONTENT_FORM_EDIT_ARTICLE'));
+		}
+
+		$title = $this->params->def('page_title', JText::_('COM_CONTENT_FORM_EDIT_ARTICLE'));
+		if ($app->getCfg('sitename_pagetitles', 0) == 1) {
+			$title = JText::sprintf('JPAGETITLE', $app->getCfg('sitename'), $title);
+		}
+		elseif ($app->getCfg('sitename_pagetitles', 0) == 2) {
+			$title = JText::sprintf('JPAGETITLE', $title, $app->getCfg('sitename'));
+		}
+		$this->document->setTitle($title);
+
+		$pathway = $app->getPathWay();
+		$pathway->addItem($title, '');
+
+		if ($this->params->get('menu-meta_description'))
+		{
+			$this->document->setDescription($this->params->get('menu-meta_description'));
+		}
+
+		if ($this->params->get('menu-meta_keywords'))
+		{
+			$this->document->setMetadata('keywords', $this->params->get('menu-meta_keywords'));
+		}
+
+		if ($this->params->get('robots'))
+		{
+			$this->document->setMetadata('robots', $this->params->get('robots'));
+		}
+	}
+}
