ad0801b3dd7376bf6fc9baab64f82d5253847105
eaxs <tkuhn@live.com>
2012-03-27 18:15:30 +0200

    Updated install/uninstall sql

---
+++ dev/4.0.x/component/admin/_uninstall/mysql/data.sql
@@ -0,0 +1,12 @@
+DELETE FROM `#__pf_comments`;
+DELETE FROM `#__pf_files`;
+DELETE FROM `#__pf_file_folders`;
+DELETE FROM `#__pf_milestones`;
+DELETE FROM `#__pf_milestone_map`;
+DELETE FROM `#__pf_projects`;
+DELETE FROM `#__pf_ref_tags`;
+DELETE FROM `#__pf_ref_users`;
+DELETE FROM `#__pf_tags`;
+DELETE FROM `#__pf_tasks`;
+DELETE FROM `#__pf_task_lists`;
+DELETE FROM `#__pf_task_map`;
---
+++ dev/4.0.x/component/admin/_uninstall/mysql/structure.sql
@@ -0,0 +1,12 @@
+DROP TABLE `#__pf_comments`;
+DROP TABLE `#__pf_files`;
+DROP TABLE `#__pf_file_folders`;
+DROP TABLE `#__pf_milestones`;
+DROP TABLE `#__pf_milestone_map`;
+DROP TABLE `#__pf_projects`;
+DROP TABLE `#__pf_ref_tags`;
+DROP TABLE `#__pf_ref_users`;
+DROP TABLE `#__pf_tags`;
+DROP TABLE `#__pf_tasks`;
+DROP TABLE `#__pf_task_lists`;
+DROP TABLE `#__pf_task_map`;
