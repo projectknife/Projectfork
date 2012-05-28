84eabf23af1150a4c59ce7706b8ff6831179a779
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-05-28 12:35:49 -0500

    - grabbing the value for complete in the Tasks model
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- dev/4.0.x/component/site/models/tasks.php
+++ dev/4.0.x/component/site/models/tasks.php
@@ -191,7 +191,7 @@ class ProjectforkModelTasks extends JModelList
 				'a.id, a.project_id, a.list_id, a.milestone_id, a.catid, a.title, '
                 . 'a.description, a.alias, a.checked_out, a.attribs, a.priority, '
 				. 'a.checked_out_time, a.state, a.access, a.created, a.created_by, '
-				. 'a.start_date, a.end_date, a.ordering, p.alias AS project_alias, '
+				. 'a.start_date, a.end_date, a.ordering, a.complete, p.alias AS project_alias, '
                 . 'tl.alias AS list_alias, m.alias AS milestone_alias'
 			)
 		);
