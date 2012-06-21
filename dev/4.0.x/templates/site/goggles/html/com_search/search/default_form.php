d2598e88b0a6c7fc8cfe363c54a92d714a0f62ab
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-06-21 10:06:29 -0500

    - adding more bootstrap classes to search override
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- dev/4.0.x/templates/site/goggles/html/com_search/search/default_form.php
+++ dev/4.0.x/templates/site/goggles/html/com_search/search/default_form.php
@@ -12,60 +12,65 @@ $lang = JFactory::getLanguage();
 $upper_limit = $lang->getUpperLimitSearchWord();
 ?>
 
-<form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_search');?>" method="post" class="form-horizontal">
-	<fieldset class="word">
-		<div class="control-group">
-			<label for="search-searchword"> <?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?> </label>
-			<div class="controls">
-				<input type="text" name="searchword" id="search-searchword" size="30" maxlength="<?php echo $upper_limit; ?>" value="<?php echo $this->escape($this->origkeyword); ?>" class="inputbox" />
-			</div>
+<form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_search');?>" method="post">
+
+	<div class="btn-toolbar">
+		<div class="btn-group pull-left">
+			<input type="text" name="searchword" placeholder="<?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?>" id="search-searchword" size="30" maxlength="<?php echo $upper_limit; ?>" value="<?php echo $this->escape($this->origkeyword); ?>" class="inputbox" />
 		</div>
-		<div class="form-actions">
-			<button name="Search" onclick="this.form.submit()" class="btn btn-primary button"><?php echo JText::_('COM_SEARCH_SEARCH');?></button>
+		<div class="btn-group pull-left">
+			<button name="Search" onclick="this.form.submit()" class="btn" rel="tooltip" title="<?php echo JText::_('COM_SEARCH_SEARCH');?>"><i class="icon-search"></i></button>
 		</div>
 		<input type="hidden" name="task" value="search" />
-	</fieldset>
+		<div class="clearfix"></div>
+	</div>
+
 	<div class="searchintro<?php echo $this->params->get('pageclass_sfx'); ?>">
 		<?php if (!empty($this->searchword)):?>
-		<p><i class="icon-list"></i> <?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', $this->total);?></p>
+		<p><?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<span class="badge badge-info">'. $this->total. '</span>');?></p>
 		<?php endif;?>
 	</div>
+
 	<fieldset class="phrases">
-		<legend><?php echo JText::_('COM_SEARCH_FOR');?> </legend>
-		<div class="phrases-box control-group"> 
-			<div class="controls">
-			<?php echo $this->lists['searchphrase']; ?> 
+		<legend><?php echo JText::_('COM_SEARCH_FOR');?>
+		</legend>
+			<div class="phrases-box">
+			<?php echo $this->lists['searchphrase']; ?>
 			</div>
-		</div>
-		<div class="ordering-box control-group">
-			<label for="ordering" class="ordering"> <?php echo JText::_('COM_SEARCH_ORDERING');?> </label>
-			<div class="controls"> <?php echo $this->lists['ordering'];?> </div>
-		</div>
+			<div class="ordering-box">
+			<label for="ordering" class="ordering">
+				<?php echo JText::_('COM_SEARCH_ORDERING');?>
+			</label>
+			<?php echo $this->lists['ordering'];?>
+			</div>
 	</fieldset>
+
 	<?php if ($this->params->get('search_areas', 1)) : ?>
-	<fieldset class="only">
+		<fieldset class="only">
 		<legend><?php echo JText::_('COM_SEARCH_SEARCH_ONLY');?></legend>
-		
 		<?php foreach ($this->searchareas['search'] as $val => $txt) :
 			$checked = is_array($this->searchareas['active']) && in_array($val, $this->searchareas['active']) ? 'checked="checked"' : '';
 		?>
-		<div class="control-group">
-		<label for="area-<?php echo $val;?>"> <?php echo JText::_($txt); ?> </label>
-		<div class="controls">
-		<label class="checkbox">
 		<input type="checkbox" name="areas[]" value="<?php echo $val;?>" id="area-<?php echo $val;?>" <?php echo $checked;?> />
-		</label>
-		</div>
-		</div>
+			<label for="area-<?php echo $val;?>">
+				<?php echo JText::_($txt); ?>
+			</label>
 		<?php endforeach; ?>
-		
-	</fieldset>
+		</fieldset>
 	<?php endif; ?>
-	<?php if ($this->total > 0) : ?>
-	<div class="form-limit control-group">
-		<label for="limit"> <?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?> </label>
-		<div class="controls"><?php echo $this->pagination->getLimitBox(); ?> </div>
-		</div>
-	<p class="counter"> <?php echo $this->pagination->getPagesCounter(); ?> </p>
-	<?php endif; ?>
+
+<?php if ($this->total > 0) : ?>
+
+	<div class="form-limit">
+		<label for="limit">
+			<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
+		</label>
+		<?php echo $this->pagination->getLimitBox(); ?>
+	</div>
+<p class="counter">
+		<?php echo $this->pagination->getPagesCounter(); ?>
+	</p>
+
+<?php endif; ?>
+
 </form>
--- dev/4.0.x/templates/site/goggles/html/com_search/search/default_results.php
+++ dev/4.0.x/templates/site/goggles/html/com_search/search/default_results.php
@@ -10,26 +10,39 @@
 defined('_JEXEC') or die;
 ?>
 
-<div class="search-results<?php echo $this->pageclass_sfx; ?>">
+<ul class="list-striped search-results<?php echo $this->pageclass_sfx; ?>">
 	<?php foreach($this->results as $result) : ?>
-	<div class="row-fluid">
-		<h3 class="result-title"> 
-			<!-- <?php echo $this->pagination->limitstart + $result->count.'. ';?> -->
-			<?php if ($result->href) :?>
-			<a href="<?php echo JRoute::_($result->href); ?>"<?php if ($result->browsernav == 1) :?> target="_blank"<?php endif;?>> <?php echo $this->escape($result->title);?> </a>
-			<?php else:?>
-			<?php echo $this->escape($result->title);?>
+		<li>
+			<?php if ($this->params->get('show_date')) : ?>
+				<span class="pull-right label label-info result-created<?php echo $this->pageclass_sfx; ?>">
+					<?php echo JText::sprintf('JGLOBAL_CREATED_DATE_ON', $result->created); ?>
+				</span>
 			<?php endif; ?>
-		</h3>
-		<?php if ($result->section) : ?>
-		<p class="result-category"> <span class="label label-info small<?php echo $this->pageclass_sfx; ?>"> <?php echo $this->escape($result->section); ?> </span> </p>
-		<?php endif; ?>
-		<p class="result-text"> <?php echo $result->text; ?> </p>
-		<?php if ($this->params->get('show_date')) : ?>
-		<p class="result-created<?php echo $this->pageclass_sfx; ?>"> <small><?php echo JText::sprintf('JGLOBAL_CREATED_DATE_ON', $result->created); ?></small> </p>
-		<?php endif; ?>
-	</div>
-	<hr />
+			<h5 class="result-title">
+				<?php echo $this->pagination->limitstart + $result->count.'. ';?>
+				<?php if ($result->href) :?>
+					<a href="<?php echo JRoute::_($result->href); ?>"<?php if ($result->browsernav == 1) :?> target="_blank"<?php endif;?>>
+						<?php echo $this->escape($result->title);?>
+					</a>
+				<?php else:?>
+					<?php echo $this->escape($result->title);?>
+				<?php endif; ?>
+				<?php if ($result->section) : ?>
+					<small class="result-category">
+						<span class="small<?php echo $this->pageclass_sfx; ?>">
+							(<?php echo $this->escape($result->section); ?>)
+						</span>
+					</small>
+				<?php endif; ?>
+			</h5>
+			
+			<div class="result-text">
+				<?php echo $result->text; ?>
+			</div>
+		</li>
 	<?php endforeach; ?>
+</ul>
+
+<div class="pagination">
+	<?php echo $this->pagination->getPagesLinks(); ?>
 </div>
-<div class="pagination"> <?php echo $this->pagination->getPagesLinks(); ?> </div>
