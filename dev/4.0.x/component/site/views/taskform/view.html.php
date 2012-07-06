eced083cfd8aa63d394777d452c14b4e71c02651
Marcel van Beelen <webmaster@sovainfo.nl>
2012-07-06 22:32:47 +0200

    Task progress percentage

--- dev/4.0.x/component/site/views/taskform/view.html.php
+++ dev/4.0.x/component/site/views/taskform/view.html.php
@@ -80,7 +80,15 @@ class ProjectforkViewTaskForm extends JView
 		// Create a shortcut to the parameters.
 		$params	= &$this->state->params;
 
-
+		$progress = explode("-",$params->get('task_progress'));
+		if (count($progress) == 3) {
+			$this->form->setFieldAttribute("complete", "type", "integer");
+				
+			$this->form->setFieldAttribute("complete", "first", "$progress[1]");
+			$this->form->setFieldAttribute("complete", "last", "$progress[2]");
+			$this->form->setFieldAttribute("complete", "step", "$progress[3]");
+		}
+		
 		//Escape strings for HTML output
 		$this->pageclass_sfx = htmlspecialchars($params->get('pageclass_sfx'));
 
