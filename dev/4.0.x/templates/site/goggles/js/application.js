fcf1983f517792a52a6f5455b38669b5707f6537
eaxs <tkuhn@live.com>
2012-07-08 14:48:17 +0200

    Fixed js error

--- dev/4.0.x/templates/site/goggles/js/application.js
+++ dev/4.0.x/templates/site/goggles/js/application.js
@@ -1,10 +1,10 @@
-$('.typeahead').typeahead()
-$('.tabs').button()
-$('*[rel=tooltip]').tooltip()
-$('*[rel=popover]').popover()
-$(".alert-message").alert()
-$(window).bind("load resize", function() {
- var windowHeight = "height:"+($(window).height()-45)+"px"; // height of full document
- var windowWidth = "width:"+($(window).width()-30)+"px"; // width of full document
- $('.side-nav, .fluid-content.main, .modal.full').attr('style', windowHeight);
+jQuery('.typeahead').typeahead()
+jQuery('.tabs').button()
+jQuery('*[rel=tooltip]').tooltip()
+jQuery('*[rel=popover]').popover()
+jQuery(".alert-message").alert()
+jQuery(window).bind("load resize", function() {
+ var windowHeight = "height:"+(jQuery(window).height()-45)+"px"; // height of full document
+ var windowWidth = "width:"+(jQuery(window).width()-30)+"px"; // width of full document
+ jQuery('.side-nav, .fluid-content.main, .modal.full').attr('style', windowHeight);
 });
