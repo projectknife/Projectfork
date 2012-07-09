e9b93a8de0468ac3123debfe14e3d82c2845778a
kyleledbetter <kyle@kyleledbetter.com>
2012-07-09 13:02:11 -0500

    - removing unnecessary js from goggles

--- dev/4.0.x/templates/site/goggles/js/application.js
+++ dev/4.0.x/templates/site/goggles/js/application.js
@@ -1,10 +1,2 @@
-jQuery('.typeahead').typeahead()
-jQuery('.tabs').button()
 jQuery('*[rel=tooltip]').tooltip()
 jQuery('*[rel=popover]').popover()
-jQuery(".alert-message").alert()
-jQuery(window).bind("load resize", function() {
- var windowHeight = "height:"+(jQuery(window).height()-45)+"px"; // height of full document
- var windowWidth = "width:"+(jQuery(window).width()-30)+"px"; // width of full document
- jQuery('.side-nav, .fluid-content.main, .modal.full').attr('style', windowHeight);
-});
