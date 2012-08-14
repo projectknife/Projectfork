9ac6d77def876f05261342c65944736dc45c79c9
kyleledbetter <kyle@kyleledbetter.com>
2012-08-14 12:07:43 -0500

    - initial Timesheet mock markup

--- dev/4.0.x/component/site/views/timesheet/tmpl/default.php
+++ dev/4.0.x/component/site/views/timesheet/tmpl/default.php
@@ -79,36 +79,138 @@ $action_count = count($this->actions);
                     </div>
                 </div>
             <?php endif; ?>
+            
+            <h3>Project Title <?php echo $this->escape($this->params->get('page_heading')); ?></h3>
+            
+            <div class="row-fluid">
+            	<div class="span3">
+            		<div class="thumbnail thumbnail-timesheet">
+            			<h6>Total Hours</h6>
+            			<h1>100.0</h1>
+            			<h5>Estimated (124.0)</h5>
+            		</div>
+            	</div>
+            	<div class="span6">
+            		<div class="thumbnail thumbnail-timesheet">
+            			<h6>Total Hours</h6>
+            			<div class="row-fluid">
+            				<div class="span6">
+            					<div class="progress progress-success">
+									<div class="bar" style="width: 60%;"></div>
+								</div>
+								<div class="progress">
+									<div class="bar" style="width: 40%;"></div>
+								</div>
+            				</div>
+            				<div class="span6">
+            					<h2>60.0 <span class="label label-success">Billable</span></h2>
+            					<h2>40.0 <span class="label label-info">Unbillable</span></h2>
+            				</div>
+            			</div>
+            		</div>
+            	</div>
+            	<div class="span3">
+            		<div class="thumbnail thumbnail-timesheet">
+            			<h6>Billable Total</h6>
+            			<h2>$2,000.00</h2>
+            			<h5>Estimated (2,500.00)</h5>
+            		</div>
+            	</div>
+            </div>
+            <hr />
+            <table class="table table-striped">
+            	<thead>
+            		<tr>
+            			<th>Task</th>
+            			<th width="5%"></th>
+            			<th width="10%">Time</th>
+            			<th width="20%"></th>
+            			<th width="10%">Author</th>
+            			<th width="10%">Date</th>
+            			<th width="10%">Rate</th>
+            			<th width="10%">Billable</th>
+            		</tr>
+            	</thead>
+            	<tbody>
+			        <?php
+			        $k = 0;
+			        foreach($this->items AS $i => $item) :
+			            $access = ProjectforkHelperAccess::getActions('time', $item->id);
+			
+			            $can_create   = $access->get('time.create');
+			            $can_edit     = $access->get('time.edit');
+			            $can_change   = $access->get('time.edit.state');
+			            $can_edit_own = ($access->get('time.edit.own') && $item->created_by == $uid);
+			
+			            /**
+			             * Available data:
+			             *
+			             * $item->id;
+			             * $item->project_title
+			             * $item->task_title
+			             * $item->description
+			             * $item->author_name
+			             * $item->access_level
+			             * $item->log_date
+			             * JHtml::_('date', $item->log_date, JText::_('DATE_FORMAT_LC4'))
+			             * JHtml::_('timesheet.format', $item->log_time)
+			             */
+			        ?>
+			        <tr>
+			        	<td>
+			        		<a href="#" rel="popover" title="<?php echo $item->task_title; ?>" data-content="<?php echo $item->description; ?>"><?php echo $item->task_title; ?></a>
+			        	</td>
+			        	<td>
+			        		<?php
+                            $this->menu->start(array('class' => 'btn-mini'));
+                            $this->menu->itemEdit('timeform', $item->id, ($can_edit || $can_edit_own));
+                            $this->menu->itemTrash('time', $i, $can_change);
+                            $this->menu->end();
 
-            <?php
-            $k = 0;
-            foreach($this->items AS $i => $item) :
-                $access = ProjectforkHelperAccess::getActions('time', $item->id);
-
-                $can_create   = $access->get('time.create');
-                $can_edit     = $access->get('time.edit');
-                $can_change   = $access->get('time.edit.state');
-                $can_edit_own = ($access->get('time.edit.own') && $item->created_by == $uid);
-
-                /**
-                 * Available data:
-                 *
-                 * $item->id;
-                 * $item->project_title
-                 * $item->task_title
-                 * $item->description
-                 * $item->author_name
-                 * $item->access_level
-                 * $item->log_date
-                 * JHtml::_('date', $item->log_date, JText::_('DATE_FORMAT_LC4'))
-                 * JHtml::_('timesheet.format', $item->log_time)
-                 */
-            ?>
-
-            <?php
-            $k = 1 - $k;
-            endforeach;
-            ?>
+                            echo $this->menu->render(array('class' => 'btn-mini'));
+	                        ?>
+			        	</td>
+			        	<td>
+			        		<?php echo JHtml::_('timesheet.format', $item->log_time); ?>
+			        	</td>
+			        	<td>
+							<div class="progress">
+								<div class="bar" style="width: 60%;"></div>
+							</div>
+			        	</td>
+			        	<td>
+			        		<?php echo $item->author_name; ?>
+			        	</td>
+			        	<td>
+			        		<?php echo JHtml::_('date', $item->log_date, JText::_('DATE_FORMAT_LC4')); ?>
+			        	</td>
+			        	<td>
+			        		$100.00
+			        	</td>
+			        	<td>
+			        		$200.00
+			        	</td>
+			        </tr>
+			
+			        <?php
+			        $k = 1 - $k;
+			        endforeach;
+			        ?>
+            	</tbody>
+            	<tfoot>
+            		<tr>
+            			<th>Totals</th>
+            			<th></th>
+            			<th>100.00</th>
+            			<th></th>
+            			<th></th>
+            			<th>
+	            		</th>
+	            		<th></th>
+	            		<th>$2,000.00</th>
+            		</tr>
+            	</tfoot>
+            </table>
 
             <div class="filters btn-toolbar">
                 <?php if ($this->pagination->get('pages.total') > 1) : ?>
@@ -117,7 +219,6 @@ $action_count = count($this->actions);
                         <?php echo $this->pagination->getPagesLinks(); ?>
                     </div>
                 <?php endif; ?>
-
                 <div class="btn-group display-limit">
                     <?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>&#160;
                     <?php echo $this->pagination->getLimitBox(); ?>
