8d8f13e207971975d71ab6fc426213d683bf3c71
eaxs <tkuhn@live.com>
2012-09-08 00:57:22 +0200

    Fixed broken image links in site dashboard module. Also cleaned up code

--- dev/4.0.x/modules/site/mod_pf_dash_buttons/helper.php
+++ dev/4.0.x/modules/site/mod_pf_dash_buttons/helper.php
@@ -1,57 +1,47 @@
 <?php
 /**
-* @package   Projectfork Dashboard Buttons
-* @copyright Copyright (C) 2012 Tobias Kuhn. All rights reserved.
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
+* @package      Projectfork Dashboard Buttons
 *
-* This file is part of Projectfork.
-*
-* Projectfork is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-*
-* Projectfork is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Projectfork. If not, see <http://www.gnu.org/licenses/gpl.html>.
+* @author       Tobias Kuhn (eaxs)
+* @copyright    Copyright (C) 2006-2012 Tobias Kuhn. All rights reserved.
+* @license      http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
 **/
 
-// no direct access
-defined('_JEXEC') or die;
+defined('_JEXEC') or die();
 
 
+/**
+ * Module helper class
+ *
+ */
 abstract class modPFdashButtonsHelper
 {
+    /**
+     * Method to get a list of available buttons
+     *
+     * @return    array    $buttons    The available buttons
+     */
     public static function getButtons()
     {
-        $app  = JFactory::getApplication();
-        $user = JFactory::getUser();
-        $pid  = $app->getUserState('com_projectfork.project.active.id', 0);
-
-        $asset  = 'com_projectfork';
-
-        if($pid) $asset .= '.project.'.$pid;
-
-
+        $access  = ProjectforkHelperAccess::getActions(NULL, 0, true);
         $buttons = array();
 
-        if($user->authorise('core.create', 'com_projectfork') || $user->authorise('project.create', 'com_projectfork')) {
+        if ($access->get('project.create')) {
             $buttons['projectform.add'] = array('label' => 'MOD_PF_DASH_BUTTONS_ADD_PROJECT',
                                                 'link'  => ProjectforkHelperRoute::getProjectsRoute());
         }
-        if($user->authorise('core.create', 'com_projectfork') || $user->authorise('milestone.create', $asset)) {
+
+        if ($access->get('milestone.create')) {
             $buttons['milestoneform.add'] = array('label' => 'MOD_PF_DASH_BUTTONS_ADD_MILESTONE',
                                                   'link'  => ProjectforkHelperRoute::getMilestonesRoute());
         }
-        if($user->authorise('core.create', 'com_projectfork') || $user->authorise('tasklist.create', $asset)) {
+
+        if ($access->get('tasklist.create')) {
             $buttons['tasklistform.add'] = array('label' => 'MOD_PF_DASH_BUTTONS_ADD_TASKLIST',
                                                  'link'  => ProjectforkHelperRoute::getTasksRoute());
         }
-        if($user->authorise('core.create', 'com_projectfork') || $user->authorise('task.create', $asset)) {
+
+        if ($access->get('task.create')) {
             $buttons['taskform.add'] = array('label' => 'MOD_PF_DASH_BUTTONS_ADD_TASK',
                                              'link'  => ProjectforkHelperRoute::getTasksRoute());
         }
--- dev/4.0.x/modules/site/mod_pf_dash_buttons/mod_pf_dash_buttons.php
+++ dev/4.0.x/modules/site/mod_pf_dash_buttons/mod_pf_dash_buttons.php
@@ -1,54 +1,37 @@
 <?php
 /**
-* @package   Projectfork Dashboard Buttons
-* @copyright Copyright (C) 2012 Tobias Kuhn. All rights reserved.
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
+* @package      Projectfork Dashboard Buttons
 *
-* This file is part of Projectfork.
-*
-* Projectfork is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-*
-* Projectfork is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Projectfork. If not, see <http://www.gnu.org/licenses/gpl.html>.
+* @author       Tobias Kuhn (eaxs)
+* @copyright    Copyright (C) 2006-2012 Tobias Kuhn. All rights reserved.
+* @license      http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
 **/
 
-// no direct access
-defined('_JEXEC') or die;
+defined('_JEXEC') or die();
+
+
+$option = JRequest::getVar('option');
+$view   = JRequest::getVar('view');
 
 // Disable this module on the "user" view if it is positioned on the dashboard
-if(stripos($module->position, 'pf-dashboard') !== false &&
-   JRequest::getVar('option') == 'com_projectfork' &&
-   JRequest::getVar('view') == 'user')
-{
+if (stripos($module->position, 'pf-dashboard') !== false && $option == 'com_projectfork' && $view == 'user') {
     return '';
 }
 
-
-if(!file_exists(JPATH_ADMINISTRATOR.'/components/com_projectfork/projectfork.php')) {
+// Stop if projectfork is not installed
+if (!file_exists(JPATH_SITE . '/components/com_projectfork/helpers/route.php')) {
     echo JText::_('MOD_PF_DASH_BUTTONS_PROJECTFORK_NOT_INSTALLED');
 }
 else {
-    if(!file_exists(JPATH_ADMINISTRATOR.'/components/com_projectfork/helpers/projectfork.php')) {
-        echo JText::_('MOD_PF_STATS_DIST_PROJECTFORK_FILE_NOT_FOUND');
-    }
-    else {
-        // Include the helper classes
-        require_once dirname(__FILE__).'/helper.php';
-        require_once JPATH_ROOT.'/components/com_projectfork/helpers/route.php';
+    // Include dependancies
+    require_once JPATH_SITE . '/components/com_projectfork/helpers/route.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_projectfork/helpers/access.php';
+    require_once dirname(__FILE__) . '/helper.php';
 
-        // Get buttons
-        $buttons = modPFdashButtonsHelper::getButtons();
+    // Get buttons
+    $buttons = modPFdashButtonsHelper::getButtons();
 
-        // Include layout
-        $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
-        require JModuleHelper::getLayoutPath('mod_pf_dash_buttons', $params->get('layout', 'default'));
-    }
+    // Include layout
+    $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+    require JModuleHelper::getLayoutPath('mod_pf_dash_buttons', $params->get('layout', 'default'));
 }
