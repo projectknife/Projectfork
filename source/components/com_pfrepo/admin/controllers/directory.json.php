a4c3aa556512e0054d49aa678264716576795a39
eaxs <tkuhn@live.com>
2012-11-09 05:17:59 +0100

    Renamed directory json controller

--- source/components/com_pfrepo/admin/controllers/directory.json.php
+++ source/components/com_pfrepo/admin/controllers/directory.json.php
@@ -11,14 +11,14 @@
 defined('_JEXEC') or die();
 
 
-jimport('projectfork.application.json.controllerform');
+jimport('projectfork.controller.form.json');
 
 
 /**
  * Projectfork Directory Form Controller
  *
  */
-class PFrepoControllerDirectory extends ProjectforkControllerFormJSON
+class PFrepoControllerDirectory extends PFControllerFormJson
 {
 
 }
