0cb1aa2970acfcfdc29902a5d5ea46804722db7f
eaxs <tkuhn@live.com>
2012-11-10 20:01:52 +0100

    Updated the task view

--- source/components/com_pftasks/site/views/task/tmpl/default.php
+++ source/components/com_pftasks/site/views/task/tmpl/default.php
@@ -18,6 +18,8 @@ $canEdit = $item->params->get('access-edit');
 $user	 = JFactory::getUser();
 $uid	 = $user->get('id');
 
+$nulldate = JFactory::getDBO()->getNullDate();
+
 $asset_name = 'com_pftasks.task.'.$this->item->id;
 $canEdit	= ($user->authorise('core.edit', $asset_name));
 $canEditOwn	= ($user->authorise('core.edit.own', $asset_name) && $this->item->created_by == $uid);
@@ -27,77 +29,78 @@ $canEditOwn	= ($user->authorise('core.edit.own', $asset_name) && $this->item->cr
         <h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
     <?php endif; ?>
 
+    <div class="btn-toolbar btn-toolbar-top">
+        <?php echo $this->toolbar;?>
+    </div>
+
     <div class="page-header">
 	    <h2><?php echo $this->escape($item->title); ?></h2>
-	</div>
-
-	<dl class="article-info dl-horizontal pull-right">
-		<dt class="project-title">
-			<?php echo JText::_('JGRID_HEADING_PROJECT');?>:
-		</dt>
-		<dd class="project-data">
-			<a href="<?php echo JRoute::_(PFprojectsHelperRoute::getDashboardRoute($item->project_slug));?>"><?php echo $item->project_title;?></a>
-		</dd>
-        <?php if($item->milestone_id) : ?>
-    		<dt class="milestone-title">
-    			<?php echo JText::_('JGRID_HEADING_MILESTONE');?>:
-    		</dt>
-    		<dd class="milestone-data">
-    			<a href="<?php echo JRoute::_(PFmilestonesHelperRoute::getMilestoneRoute($item->project_slug, $item->milestone_slug));?>"><?php echo $item->milestone_title;?></a>
-    		</dd>
-        <?php endif; ?>
-        <?php if($item->list_id) : ?>
-    		<dt class="list-title">
-    			<?php echo JText::_('JGRID_HEADING_TASKLIST');?>:
-    		</dt>
-    		<dd class="list-data">
-    			<a href="<?php echo JRoute::_(PFtasksHelperRoute::getTasksRoute($item->project_slug, $item->milestone_slug, $item->list_slug));?>">
-                    <?php echo $item->list_title;?>
-                </a>
-    		</dd>
-        <?php endif; ?>
-        <?php if($item->start_date != JFactory::getDBO()->getNullDate()): ?>
-    		<dt class="start-title">
-    			<?php echo JText::_('JGRID_HEADING_START_DATE');?>:
-    		</dt>
-    		<dd class="start-data">
-    			<?php echo JHtml::_('date', $this->item->start_date, $this->escape( $params->get('date_format', JText::_('DATE_FORMAT_LC1'))));?>
-    		</dd>
-        <?php endif; ?>
-        <?php if($item->end_date != JFactory::getDBO()->getNullDate()): ?>
-    		<dt class="due-title">
-    			<?php echo JText::_('JGRID_HEADING_DEADLINE');?>:
-    		</dt>
-    		<dd class="due-data">
-    			<?php echo JHtml::_('date', $item->end_date, $this->escape( $params->get('date_format', JText::_('DATE_FORMAT_LC1'))));?>
-    		</dd>
-        <?php endif; ?>
-		<dt class="owner-title">
-			<?php echo JText::_('JGRID_HEADING_CREATED_BY');?>:
-		</dt>
-		<dd class="owner-data">
-			 <?php echo $this->escape($this->item->author);?>
-		</dd>
-	</dl>
-	<div class="actions btn-toolbar">
-		<div class="btn-group">
-			<?php if($canEdit || $canEditOwn) : ?>
-			   <a class="btn" href="<?php echo JRoute::_('index.php?option=com_pftasks&task=taskform.edit&id='.intval($item->id).':'.$item->alias);?>">
-			       <i class="icon-edit"></i> <?php echo JText::_('COM_PROJECTFORK_ACTION_EDIT');?>
-			   </a>
-			<?php endif; ?>
-			<!--<a href="#" class="btn"><i class="icon-print"></i> Print</a>
-			<a href="#" class="btn"><i class="icon-envelope"></i> Email</a>
-			<a href="#comments" class="btn"><i class="icon-comment"></i> Comment <span class="badge badge-warning">4</span></a>-->
-            <?php echo $item->event->afterDisplayTitle;?>
-		</div>
 	</div>
 
     <?php echo $item->event->beforeDisplayContent;?>
 
 	<div class="item-description">
 		<?php echo $item->text; ?>
+
+        <dl class="article-info dl-horizontal pull-right">
+    		<dt class="project-title">
+    			<?php echo JText::_('JGRID_HEADING_PROJECT');?>:
+    		</dt>
+    		<dd class="project-data">
+    			<a href="<?php echo JRoute::_(PFprojectsHelperRoute::getDashboardRoute($item->project_slug));?>"><?php echo $item->project_title;?></a>
+    		</dd>
+            <?php if($item->milestone_id) : ?>
+        		<dt class="milestone-title">
+        			<?php echo JText::_('JGRID_HEADING_MILESTONE');?>:
+        		</dt>
+        		<dd class="milestone-data">
+        			<a href="<?php echo JRoute::_(PFmilestonesHelperRoute::getMilestoneRoute($item->project_slug, $item->milestone_slug));?>"><?php echo $item->milestone_title;?></a>
+        		</dd>
+            <?php endif; ?>
+            <?php if($item->list_id) : ?>
+        		<dt class="list-title">
+        			<?php echo JText::_('JGRID_HEADING_TASKLIST');?>:
+        		</dt>
+        		<dd class="list-data">
+        			<a href="<?php echo JRoute::_(PFtasksHelperRoute::getTasksRoute($item->project_slug, $item->milestone_slug, $item->list_slug));?>">
+                        <?php echo $item->list_title;?>
+                    </a>
+        		</dd>
+            <?php endif; ?>
+            <?php if($item->start_date != $nulldate): ?>
+        		<dt class="start-title">
+        			<?php echo JText::_('JGRID_HEADING_START_DATE');?>:
+        		</dt>
+        		<dd class="start-data">
+        		  <?php echo JHtml::_('pfhtml.label.datetime', $item->start_date); ?>
+        		</dd>
+            <?php endif; ?>
+            <?php if($item->end_date != $nulldate): ?>
+        		<dt class="due-title">
+        			<?php echo JText::_('JGRID_HEADING_DEADLINE');?>:
+        		</dt>
+        		<dd class="due-data">
+        			<?php echo JHtml::_('pfhtml.label.datetime', $item->end_date); ?>
+        		</dd>
+            <?php endif; ?>
+    		<dt class="owner-title">
+    			<?php echo JText::_('JGRID_HEADING_CREATED_BY');?>:
+    		</dt>
+    		<dd class="owner-data">
+    			 <?php echo JHtml::_('pfhtml.label.author', $item->author, $item->created); ?>
+    		</dd>
+            <?php if (PFApplicationHelper::enabled('com_pfrepo') && count($item->attachments)) : ?>
+                <dt class="attachment-title">
+        			<?php echo JText::_('COM_PROJECTFORK_FIELDSET_ATTACHMENTS'); ?>:
+        		</dt>
+        		<dd class="attachment-data">
+                     <?php echo JHtml::_('pfrepo.attachments', $item->attachments); ?>
+        		</dd>
+            <?php endif; ?>
+    	</dl>
+        <div class="clearfix"></div>
 	</div>
+
 	<hr />
 
     <?php echo $item->event->afterDisplayContent;?>
