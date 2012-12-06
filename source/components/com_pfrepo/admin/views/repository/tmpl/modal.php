eb2d926a765073e4abe27d10dd490a1e80ce51d4
eaxs <tkuhn@live.com>
2012-12-06 09:56:21 +0100

    Fixed: Modal Attachments window looking messed up in Joomla 3.0. Closes #68

--- source/components/com_pfrepo/admin/views/repository/tmpl/modal.php
+++ source/components/com_pfrepo/admin/views/repository/tmpl/modal.php
@@ -27,38 +27,49 @@ $access      = PFrepoHelper::getActions('directory', $this_dir->id);
 <form action="<?php echo JRoute::_('index.php?option=com_pfrepo&view=repository' . $link_append); ?>" method="post" name="adminForm" id="adminForm" enctype="multipart/form-data">
 
     <fieldset id="filter-bar">
-        <div class="filter-search fltlft">
-            <label class="filter-search-lbl" for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
-            <input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" />
-
-            <button type="submit" class="btn"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-            <button type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+        <div class="filter-search fltlft btn-toolbar pull-left">
+            <div class="fltlft btn-group pull-left">
+                <input type="text" name="filter_search" id="filter_search" class="inputbox input-medium" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" />
+            </div>
+            <div class="fltlft btn-group pull-left">
+                <button type="submit" class="btn"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+                <button type="button" class="btn" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+            </div>
         </div>
-        <div class="filter-select fltrt">
-            <?php if ($project) : ?>
-                <select name="filter_parent_id" class="inputbox" onchange="this.form.submit()">
-                    <option value=""><?php echo JText::_('JOPTION_SELECT_DIRECTORY');?></option>
-                    <?php echo JHtml::_('select.options', JHtml::_('pfrepo.pathOptions', $project), 'value', 'text', $this->state->get('filter.parent_id'));?>
-            </select>
-            <?php endif; ?>
+        <div class="filter-select fltrt btn-toolbar pull-right">
+            <div class="fltrt btn-group">
+                <?php if ($project) : ?>
+                    <select name="filter_parent_id" class="inputbox" onchange="this.form.submit()">
+                        <option value=""><?php echo JText::_('JOPTION_SELECT_DIRECTORY');?></option>
+                        <?php echo JHtml::_('select.options', JHtml::_('pfrepo.pathOptions', $project), 'value', 'text', $this->state->get('filter.parent_id'));?>
+                </select>
+                <?php endif; ?>
+            </div>
         </div>
     </fieldset>
 
-    <div class="clr"></div>
+    <div class="clr clearfix"></div>
 
     <?php if ($access->get('core.create')) : ?>
+        <hr />
         <fieldset id="upload-form">
-            <input type="file" name="jform[file]" id="jform_file"/>
-            <button type="button" class="btn" onclick="Joomla.submitbutton('file.save');"><?php echo JText::_('JACTION_UPLOAD'); ?></button>
+            <div class="filter-select fltrt btn-toolbar pull-right">
+                <div class="fltlft btn-group pull-left">
+                    <input type="file" name="jform[file]" id="jform_file"/>
+                </div>
+                <div class="fltlft btn-group pull-left">
+                    <button type="button" class="btn" onclick="Joomla.submitbutton('file.save');"><?php echo JText::_('JACTION_UPLOAD'); ?></button>
+                </div>
+            </div>
             <input type="hidden" name="jform[dir_id]" id="jform_dir_id" value="<?php echo $this->escape((int) $this_dir->id);?>"/>
             <input type="hidden" name="jform[project_id]" id="jform_dir_id" value="<?php echo $this->escape((int) $this->state->get('filter.project'));?>"/>
             <input type="hidden" name="jform[access]" id="jform_access" value="<?php echo $this->escape((int) $this_dir->access);?>"/>
         </fieldset>
     <?php endif; ?>
 
-    <div class="clr"></div>
+    <div class="clr clearfix"></div>
 
-    <table class="adminlist">
+    <table class="adminlist table table-striped">
         <thead>
             <tr>
                 <th width="1%">
