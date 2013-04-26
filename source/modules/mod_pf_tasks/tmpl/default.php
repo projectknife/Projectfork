112ccd041ff5a218a8e895b1127df06ee27de8d8
kyleledbetter <kyle@kyleledbetter.com>
2013-04-26 11:51:23 -0500

    Updating task layout, avatar and date

--- source/modules/mod_pf_tasks/tmpl/default.php
+++ source/modules/mod_pf_tasks/tmpl/default.php
@@ -14,8 +14,44 @@ defined('_JEXEC') or die();
 $show_date     = $params->get('show_deadline');
 $show_assigned = $params->get('show_assigned');
 $show_priority = $params->get('show_priority');
+
+if ($show_priority) :
+    $doc =& JFactory::getDocument();
+    $style = '.complete {'
+            . 'opacity:0.5;'
+            . '}'
+            . '.task-title > a {'
+            . 'margin-left:10px;'
+            . 'margin-right:10px;'
+            . '}'
+            . '.margin-none {'
+            . 'margin: 0;'
+            . '}'
+            . '.priority-1 {'
+            . 'border-left:2px solid #CCC;'
+            . '}'
+            . '.priority-2 {'
+            . 'border-left:2px solid #468847;'
+            . '}'
+            . '.priority-3 {'
+            . 'border-left:2px solid #3a87ad;'
+            . '}'
+            . '.priority-4 {'
+            . 'border-left:2px solid #c09853;'
+            . '}'
+            . '.priority-5 {'
+            . 'border-left:2px solid #b94a48;'
+            . '}'
+            . '.row-striped.row-tasks {'
+            . 'line-height: 30px;'
+            . '}'
+            . '.row-striped .img-circle {'
+            . 'margin: 0 10px 0 0;'
+            . '}';
+    $doc->addStyleDeclaration( $style );
+endif;
 ?>
-<ul class="list-striped list-condensed">
+<div class="row-striped row-tasks">
     <?php
     foreach ($items AS $i => $item) :
         // Deadline and completition date
@@ -26,37 +62,42 @@ $show_priority = $params->get('show_priority');
             ($item->complete ? array('past-class' => 'label-success', 'past-icon' => 'calendar') : array())
         );
         ?>
-        <li>
-            <span class="task-name item-name">
-                <?php
-                if ($show_date) :
-                    if ($item->complete) :
-                        echo '<i class="icon-checkbox"></i>';
-                    else :
-                        echo '<i class="icon-checkbox-unchecked"></i>';
-                    endif;
-                endif;
+        <div class="row-fluid priority-<?php echo $item->priority; ?>">
+            <div class="span12">
+                <?php if ($show_assigned) :
+                    foreach ($item->users AS $usr) :
+                        $usr->user_id;
+                        $usr->username;
+                        $usr->name;
+                    endforeach;
                 ?>
-                <a href="<?php echo JRoute::_(PFtasksHelperRoute::getTaskRoute($item->slug, $item->project_slug, $item->milestone_slug, $item->list_slug));?>">
-                    <?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>
-                </a>
-            </span>
-            <span class="pull-right">
-                <?php
-                    if ($show_assigned) :
-                        echo JHtml::_('pftasks.assignedLabel', $item->id, $i, $item->users) . ' ';
-                    endif;
-
-                    if ($show_priority) :
-                        echo JHtml::_('pftasks.priorityLabel', $item->id, $i, $item->priority) . ' ';
-                    endif;
-
+                    <img title="<?php echo $usr->name;?>"
+                         width="30"
+                         src="<?php echo JHtml::_('projectfork.avatar.path', $usr->user_id);?>"
+                         class="img-circle hasTooltip pull-left width-30"
+                    />
+                <?php endif; ?>
+                <span class="task-name item-name">
+                    <?php
                     if ($show_date) :
-                        echo $date;
+                        if ($item->complete) :
+                            echo '<i class="icon-checkbox"></i>';
+                        else :
+                            echo '<i class="icon-checkbox-unchecked"></i>';
+                        endif;
+                    endif;
+                    ?>
+                    <a href="<?php echo JRoute::_(PFtasksHelperRoute::getTaskRoute($item->slug, $item->project_slug, $item->milestone_slug, $item->list_slug));?>">
+                        <?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>
+                    </a>
+                </span>
+                <?php
+                    if ($show_date) :
+                        echo '<span class="pull-right small muted">' . JHtml::_('date', $item->end_date, JText::_('M d')) . '</span>';
                     endif;
                 ?>
-            </span>
-            <div class="clearfix"></div>
-        </li>
+                <div class="clearfix"></div>
+            </div>
+        </div>
     <?php endforeach; ?>
-</ul>
+</div>
