a60a24adc5060569719ae833f88446dcc5997c74
eaxs <tkuhn@live.com>
2013-04-26 23:00:36 +0200

    Added date helper function to align a time span on a contrained time line

--- source/libraries/projectfork/date/date.php
+++ source/libraries/projectfork/date/date.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* @package      Projectfork.Library
-* @subpackage   Date
+* @package      pkg_projectfork
+* @subpackage   lib_projectfork
 *
 * @author       Tobias Kuhn (eaxs)
-* @copyright    Copyright (C) 2006-2012 Tobias Kuhn. All rights reserved.
+* @copyright    Copyright (C) 2006-2013 Tobias Kuhn. All rights reserved.
 * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
 **/
 
@@ -68,5 +68,174 @@ abstract class PFDate
         }
 
         return $format;
+    }
+
+
+    /**
+     * Method to shift a time span along a constrained time line
+     *
+     * @param     array    $timespan           The time span to adjust
+     * @param     array    $constraint         Constrained time line
+     * @param     array    $prev_constraint    Previous time line
+     * @param     array    $options            Config options
+     *
+     * @return    array    $result             The update start and end date
+     */
+    public static function shiftTimeline($timespan, $constraint = array(), $prev_constraint = array(), $options = array())
+    {
+        static $db = null;
+        static $nd = null;
+
+        if (is_null($db)) {
+            $db = JFactory::getDbo();
+            $nd = $db->getNullDate();
+        }
+
+        // Prepare current constraint vars
+        $start = (isset($constraint[0]) ? $constraint[0] : null);
+        $end   = (isset($constraint[1]) ? $constraint[1] : null);
+
+        $has_start  = !(empty($start) || $start == $nd);
+        $has_end    = !(empty($end)   || $end == $nd);
+        $start_time = ($has_start ? strtotime($start) : 0);
+        $end_time   = ($has_end   ? strtotime($end)   : 0);
+        $time_span  = ($has_start && $has_end) ? $end_time - $start_time : 0;
+
+        // Prepare previous constraint vars
+        $prev_start = (isset($prev_constraint[0]) ? $prev_constraint[0] : null);
+        $prev_end   = (isset($prev_constraint[1]) ? $prev_constraint[1] : null);
+
+        $prev_has_start  = !(empty($prev_start) || $prev_start == $nd);
+        $prev_has_end    = !(empty($prev_end)   || $prev_end == $nd);
+        $prev_start_time = ($prev_has_start ? strtotime($prev_start) : 0);
+        $prev_end_time   = ($prev_has_end   ? strtotime($prev_end)   : 0);
+        $prev_time_span  = ($prev_has_start && $prev_has_end) ? $prev_end_time - $prev_start_time : 0;
+
+        // Prepare timeline item vars
+        $item_start_date = (isset($timespan[0]) ? $timespan[0] : null);
+        $item_end_date   = (isset($timespan[1]) ? $timespan[1] : null);
+
+        $item_has_start  = !(empty($item_start_date) || $item_start_date == $nd);
+        $item_has_end    = !(empty($item_end_date)   || $item_end_date == $nd);
+        $item_start_time = ($item_has_start ? strtotime($item_start_date) : 0);
+        $item_end_time   = ($item_has_end   ? strtotime($item_end_date)   : 0);
+        $item_time_span  = ($item_has_start && $item_has_end) ? $item_end_time - $item_start_time : 0;
+
+        // Calculate the offsets
+        $item_offset_start_to_start = ($has_start && $item_has_start) ? $item_start_time - $start_time : 0;
+        $item_offset_start_to_end   = ($has_end && $item_has_start)   ? $end_time - $item_start_time   : 0;
+        $item_offset_end_to_end     = ($has_end && $item_has_end)     ? $end_time - $item_end_time     : 0;
+        $item_offset_end_to_start   = ($has_start && $item_has_end)   ? $item_end_time - $start_time   : 0;
+
+        // Start date offset
+        $item_offset_start = 0;
+
+        if ($item_offset_start_to_start < 0) {
+            $item_offset_start = $item_offset_start_to_start;
+        }
+
+        if ($item_offset_start_to_end < 0 && $item_offset_start_to_end < $item_offset_start) {
+            $item_offset_start = $item_offset_start_to_end;
+        }
+
+        // End date offset
+        $item_offset_end = 0;
+
+        if ($item_offset_end_to_end < 0) {
+            $item_offset_end = $item_offset_end_to_end;
+        }
+
+        if ($item_offset_end_to_start < 0 && $item_offset_end_to_start < $item_offset_end) {
+            $item_offset_end = $item_offset_end_to_start;
+        }
+
+        if ($item_offset_start < 0 && $item_offset_end < 0) {
+            // Time span is entirely out of bounds
+
+            // Can we can preserve the time span duration?
+            if ($item_time_span <= $time_span && $item_time_span > 0) {
+                // Did the previous constraint have a set time span?
+                if ($prev_time_span) {
+                    $item_start_offset = $item_start_time - $prev_start_time;
+                    $item_end_offset   = $prev_end_time - $item_end_time;
+
+                    // Shift the entire span relative along the contrained timeline if possible
+                    if ($item_time_span + $item_start_offset <= $time_span) {
+                        // Shift by start date offset
+                        $item_start_time = $start_time + $item_start_offset;
+                        $item_end_time   = $item_start_time + $item_time_span;
+                    }
+                    elseif ($item_time_span + $item_end_offset <= $time_span) {
+                        // Shift by end date offset
+                        $item_end_time   = $end_time - $item_end_offset;
+                        $item_start_time = $item_end_time - $item_time_span;
+                    }
+                    else {
+                        // Dont shift
+                        $item_start_time = $start_time;
+                        $item_end_time   = $item_start_time + $item_time_span;
+                    }
+                }
+                else {
+                    // No previous time line
+                    $item_start_time = $start_time;
+                    $item_end_time   = $item_start_time + $item_time_span;
+                }
+            }
+            else {
+                // Unable to preserve duration
+                $item_start_time = $start_time;
+                $item_end_time   = $end_time;
+            }
+        }
+        elseif ($item_offset_start < 0) {
+            // Start date is out of bounds
+
+            // Cut it off
+            if ($has_start) {
+                $item_start_time = $start_time;
+            }
+            elseif ($has_end) {
+                $item_start_time = $end_time;
+            }
+            else {
+                $item_start_time = 0;
+            }
+        }
+        elseif ($item_offset_end < 0) {
+            // End date is out of bounds
+
+            // Cut it off
+            if ($has_end) {
+                $item_end_time = $end_time;
+            }
+            elseif ($has_start) {
+                $item_end_time = $start_time;
+            }
+            else {
+                $item_end_time = 0;
+            }
+        }
+
+        // Prepare return data
+        $result = array();
+
+        if ($item_has_start && strtotime($item_start_date) != $item_start_time) {
+            $date = new JDate(($item_start_time == 0 ? $nd : $item_start_time));
+            $result[0] = $date->toSql();
+        }
+        else {
+            $result[0] = $item_start_date;
+        }
+
+        if ($item_has_end && strtotime($item_end_date) != $item_end_time) {
+            $date = new JDate(($item_end_time == 0 ? $nd : $item_end_time));
+            $result[1] = $date->toSql();
+        }
+        else {
+            $result[1] = $item_end_date;
+        }
+
+        return $result;
     }
 }
