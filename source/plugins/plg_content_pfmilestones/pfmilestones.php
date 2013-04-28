ff99dfbeec70ece60bccffe8aa44c7ce218c4548
eaxs <tkuhn@live.com>
2013-04-28 11:53:24 +0200

    Added tasks content plugin

--- source/plugins/plg_content_pfmilestones/pfmilestones.php
+++ source/plugins/plg_content_pfmilestones/pfmilestones.php
@@ -23,65 +23,8 @@ class plgContentPFmilestones extends JPlugin
      * @var    array
      */
     protected $contexts = array(
-        'com_pfprojects.project', 'com_pfprojects.form',
-        'com_pfmilestones.milestone', 'com_pfmilestones.form'
+        'com_pfprojects.project', 'com_pfprojects.form'
     );
-
-    /**
-     * Start date before updating
-     *
-     * @var    string
-     */
-    protected $prev_start;
-
-    /**
-     * End date before updating
-     *
-     * @var    string
-     */
-    protected $prev_end;
-
-
-    /**
-     * "onContentBeforeSave" event handler
-     *
-     * @param     string     $context    The item context
-     * @param     object     $table      The item table object
-     * @param     boolean    $is_new     New item indicator (True is new, False is update)
-     *
-     * @return    boolean                True
-     */
-    public function onContentBeforeSave($context, $table, $is_new = false)
-    {
-        // Do nothing if the plugin is disabled
-        if (!JPluginHelper::isEnabled('content', 'pfmilestones')) {
-            return true;
-        }
-
-        // Check if the context is supported
-        if (!in_array($context, $this->contexts)) {
-            return true;
-        }
-
-        if (in_array($context, array('com_pfmilestones.milestone', 'com_pfmilestones.form'))) {
-            $db    = JFactory::getDbo();
-            $query = $db->getQuery(true);
-
-            $query->select('start_date, end_date')
-                  ->from('#__pf_milestones')
-                  ->where('id = ' . (int) $table->id);
-
-            $db->setQuery($query);
-            $item = $db->loadObject();
-
-            if (!empty($item)) {
-                $this->prev_start = $item->start_date;
-                $this->prev_end   = $item->end_date;
-            }
-        }
-
-        return true;
-    }
 
 
     /**
@@ -104,26 +47,11 @@ class plgContentPFmilestones extends JPlugin
         // Do nothing if this is a new item
         if ($is_new) return true;
 
+        // Update access
+        $this->updateAccess($table->id, $table->access);
 
-        if (in_array($context, array('com_pfprojects.project', 'com_pfprojects.form'))) {
-            // Update access
-            $this->updateAccess($table->id, $table->access);
-
-            // Update publishing state
-            $this->updatePubState($table->id, $table->state);
-        }
-
-
-        if (in_array($context, array('com_pfmilestones.milestone', 'com_pfmilestones.form'))) {
-            $start_changed = ($table->start_date != $this->prev_start);
-            $end_changed   = ($table->end_date != $this->prev_end);
-
-            // Update start and end dates on milestones and tasks
-            if ($start_changed || $end_changed) {
-                $this->updateTimeline($table->id, $table->start_date, $table->end_date);
-            }
-        }
-
+        // Update publishing state
+        $this->updatePubState($table->id, $table->state);
 
         return true;
     }
@@ -143,12 +71,13 @@ class plgContentPFmilestones extends JPlugin
         // Do nothing if the plugin is disabled
         if (!JPluginHelper::isEnabled('content', 'pfmilestones')) return true;
 
-        if (in_array($context, array('com_pfprojects.project', 'com_pfprojects.form'))) {
-            // Update publishing state
-            foreach ($pks AS $id)
-            {
-                $this->updatePubState($id, $value);
-            }
+        // Check if the context is supported
+        if (!in_array($context, $this->contexts)) return true;
+
+        // Update publishing state
+        foreach ($pks AS $id)
+        {
+            $this->updatePubState($id, $value);
         }
 
         return true;
@@ -165,15 +94,14 @@ class plgContentPFmilestones extends JPlugin
      */
     public function onContentAfterDelete($context, $table)
     {
-
-
         // Do nothing if the plugin is disabled
         if (!JPluginHelper::isEnabled('content', 'pfmilestones')) return true;
 
-        if (in_array($context, array('com_pfprojects.project', 'com_pfprojects.form'))) {
-            // Delete milestones
-            $this->deleteFromProject($table->id);
-        }
+        // Check if the context is supported
+        if (!in_array($context, $this->contexts)) return true;
+
+        // Delete milestones
+        $this->deleteFromProject($table->id);
 
         return true;
     }
@@ -281,83 +209,5 @@ class plgContentPFmilestones extends JPlugin
 
         $db->setQuery($query);
         $db->execute();
-    }
-
-
-    /**
-     * Method to update the start and end dates of tasks
-     * associated with the milestone.
-     *
-     * @param     integer    $id       The milestone id
-     * @param     string     $start    The project start date
-     * @param     string     $end      The project end date
-     *
-     * @return    void
-     */
-    protected function updateTimeline($id, $start = null, $end = null)
-    {
-        jimport('projectfork.library');
-
-        $db    = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $nd    = $db->getNullDate();
-
-        $has_start  = !(empty($start) || $start == $nd);
-        $has_end    = !(empty($end)   || $end == $nd);
-
-        // Do nothing if no start and end date are set
-        if (!$has_start && !$has_end) return;
-
-        $this_const = array($start, $end);
-        $prev_const = array($this->prev_start, $this->prev_end);
-
-        // Get all tasks
-        $query->select('id, start_date, end_date')
-              ->from('#__pf_tasks')
-              ->where('milestone_id = ' . (int) $id)
-              ->order('id ASC');
-
-        $db->setQuery($query);
-        $tasks = (array) $db->loadObjectList('id');
-
-        // Process tasks
-        foreach ($tasks AS $i => $item)
-        {
-            $item_has_start = !(empty($item->start_date) || $item->start_date == $nd);
-            $item_has_end   = !(empty($item->end_date)   || $item->end_date == $nd);
-
-            // Skip if not start and end date are set
-            if (!$item_has_start && !$item_has_end) continue;
-
-            $span = array($item->start_date, $item->end_date);
-
-            // Shift dates
-            list($new_start, $new_end) = PFDate::shiftTimeline($span, $this_const, $prev_const);
-
-            // Update the database
-            $updates = array();
-
-            if ($item_has_start && $item->start_date != $new_start) {
-                $updates[] = 'start_date = ' . $db->quote($new_start);
-
-                $milestones[$i]->start_date = $new_start;
-            }
-
-            if ($item_has_end && $item->end_date != $new_end) {
-                $updates[] = 'end_date = ' . $db->quote($new_end);
-
-                $milestones[$i]->end_date = $new_end;
-            }
-
-            if (count($updates)) {
-                $query->clear()
-                      ->update('#__pf_tasks')
-                      ->set(implode(', ', $updates))
-                      ->where('id = ' . (int) $item->id);
-
-                $db->setQuery($query);
-                $db->execute();
-            }
-        }
     }
 }
---
+++ source/plugins/plg_content_pftasks/script.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @package      pkg_projectfork
+ * @subpackage   plg_content_pftasks
+ *
+ * @author       Tobias Kuhn (eaxs)
+ * @copyright    Copyright (C) 2006-2013 Tobias Kuhn. All rights reserved.
+ * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
+ */
+
+defined('_JEXEC') or die();
+
+
+class plgContentPFtasksInstallerScript
+{
+    /**
+     * Called before any type of action
+     *
+     * @param     string              $route      Which action is happening (install|uninstall|discover_install)
+     * @param     jadapterinstance    $adapter    The object responsible for running this script
+     *
+     * @return    boolean                         True on success
+     */
+    public function preflight($route, JAdapterInstance $adapter)
+    {
+        if (strtolower($route) == 'install' || strtolower($route) == 'update') {
+            if (!defined('PF_LIBRARY')) {
+                jimport('projectfork.library');
+            }
+
+            $name = htmlspecialchars($adapter->get('manifest')->name, ENT_QUOTES, 'UTF-8');
+
+            // Check if the library is installed
+            if (!defined('PF_LIBRARY')) {
+                JError::raiseWarning(1, JText::_('This extension (' . $name . ') requires the Projectfork Library to be installed!'));
+                return false;
+            }
+
+            // Check if the projectfork component is installed
+            if (!PFApplicationHelper::exists('com_projectfork')) {
+                JError::raiseWarning(1, JText::_('This extension (' . $name . ') requires the Projectfork Component to be installed!'));
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Called after any type of action
+     *
+     * @param     string              $route      Which action is happening (install|uninstall|discover_install)
+     * @param     jadapterinstance    $adapter    The object responsible for running this script
+     *
+     * @return    boolean                         True on success
+     */
+    public function postflight($route, JAdapterInstance $adapter)
+    {
+        if (strtolower($route) == 'install') {
+            // Get the XML manifest data
+            $manifest = $adapter->get('manifest');
+
+            // Get plugin published state
+            $name  = $manifest->name;
+            $state = (isset($manifest->published) ? (int) $manifest->published : 0);
+
+            if ($state) {
+                PFInstallerHelper::publishPlugin($name, $state);
+            }
+        }
+
+        return true;
+    }
+}
