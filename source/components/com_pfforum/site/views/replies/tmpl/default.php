7896082046806468c978bfd0ccca6a41d38504d1
eaxs <tkuhn@live.com>
2013-07-22 15:27:35 +0200

    Fixed: Attachments for topics and replies not showing. Closes #648

--- source/components/com_pfforum/site/views/replies/tmpl/default.php
+++ source/components/com_pfforum/site/views/replies/tmpl/default.php
@@ -117,6 +117,12 @@ Joomla.submitbutton = function(task)
     						<span class="small muted pull-right"><?php echo JHtml::_('date', $this->topic->created, $this->params->get('date_format', JText::_('DATE_FORMAT_LC2'))); ?></span>
     						<div class="well-description">
     							<?php echo $this->topic->description; ?>
+                                <?php if (count($this->topic->attachment)) : ?>
+                                    <fieldset>
+                                        <legend class="small" style="font-weight: bold;"><?php echo JText::_('COM_PROJECTFORK_FIELDSET_ATTACHMENTS'); ?></legend>
+                                        <?php echo JHtml::_('pfrepo.attachments', $this->topic->attachment); ?>
+                                    </fieldset>
+                                <?php endif; ?>
     						</div>
     					</div>
                         <div class="btn-toolbar margin-none">
@@ -162,6 +168,12 @@ Joomla.submitbutton = function(task)
     						<span class="small muted pull-right"><?php echo JHtml::_('date', $item->created, $this->params->get('date_format', JText::_('DATE_FORMAT_LC2'))); ?></span>
     						<div class="well-description">
     							<?php echo $item->description;?>
+                                <?php if (count($item->attachment)) : ?>
+                                    <fieldset>
+                                        <legend class="small" style="font-weight: bold;"><?php echo JText::_('COM_PROJECTFORK_FIELDSET_ATTACHMENTS'); ?></legend>
+                                        <?php echo JHtml::_('pfrepo.attachments', $item->attachment); ?>
+                                    </fieldset>
+                                <?php endif; ?>
     						</div>
     					</div>
     					<div class="btn-toolbar margin-none">
