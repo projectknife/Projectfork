3f8ab6cfe3712b65297ba731117b4a25d4af4fb4
eaxs <tkuhn@live.com>
2013-09-11 10:22:09 +0200

    Fixed: Permission Add/Remove user input field too small in Joomla 2.5 backend. Fixed: Fatal error in permission add/remove user input ajax call when running Joomla 2.5

--- source/components/com_pfusers/admin/models/groupusers.php
+++ source/components/com_pfusers/admin/models/groupusers.php
@@ -11,7 +11,7 @@
 defined('_JEXEC') or die();
 
 
-jimport('joomla.application.component.modelitem');
+jimport('joomla.application.component.modellist');
 jimport('projectfork.application.helper');
 
 /**
